adbac4401476e3230d775e1b92bdf45afa4087e6
ZaaCoDe <60171098+AhmadSultanMA@users.noreply.github.com>
2022-06-01 20:30:08 +0700

    .

--- app/Http/Controllers/KursusController.php
+++ app/Http/Controllers/KursusController.php
@@ -29,7 +29,7 @@ class KursusController extends Controller
             return response()->json([
                 'status' => 'berhasil',
                 'data'=>$data,
-                'materi' => $res,
+                $res,
             ]);
         }
     }
